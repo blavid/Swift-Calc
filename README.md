7d0fbde1e69e02e0d1528eca1794b21b321fe7ad
Blake Clough <Blake.Clough@Webtrends.com>
2015-07-12 16:12:24 -0700

    End of Lecture 1, basic buttons are functional

---
+++ README.md
@@ -0,0 +1,5 @@
+# Swift-Calc
+
+This repo is set up to capture all phases of development for the iTunes University course on Swift development, ***Developing Applications for iOS***.
+
+As the application is built, I will commit and push to this repo at each development phase. To view them in order, simply roll back to the version you wish to see.
--- Swift-Calc/ViewController.swift
+++ Swift-Calc/ViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+
+    @IBAction func appendDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
