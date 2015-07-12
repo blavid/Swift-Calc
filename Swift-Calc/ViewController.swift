f44a4c13a2237fac0db1eb9ce69bdf644208ff3c
Blake Clough <Blake.Clough@Webtrends.com>
2015-07-12 15:43:53 -0700

    Initial Commit

---
+++ Swift-Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Calc
+//
+//  Created by Blake Clough on 7/12/15.
+//  Copyright (c) 2015 Blake Clough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
